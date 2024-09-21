a3c6a6dea70ecc823455ed443fcb31a6ba144a72
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2024-09-21 16:01:00 -0400

    passes all tests

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+};
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+};
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;  
+};
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+};
+
+function appendCat(name) {
+    const newArray = [...cats, name];
+    return newArray;
+};
+
+function prependCat(name) {
+    const newArray = [name, ...cats];
+    return newArray;
+};
+
+function removeLastCat() {
+    const newArray = [...cats];
+    newArray.pop();
+    return newArray;
+};
+
+function removeFirstCat() {
+    const newArray = [...cats];
+    newArray.shift();
+    return newArray;
+};
